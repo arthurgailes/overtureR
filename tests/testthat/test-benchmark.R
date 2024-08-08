c61c6116163c4a785d2b66178f47ad654c79e74b
Arthur Gailes <agailes1@gmail.com>
2024-08-07 20:40:14 -0400

    rm bad benchmark

--- tests/testthat/test-benchmark.R
+++ tests/testthat/test-benchmark.R
@@ -9,15 +9,15 @@ test_that("Configuration is fast", {
   expect_lt(redo_timer[["elapsed"]], 0.5)
 })
 
-test_that("stage_conn will pass CRAN", {
-  # cran requires user time <= 2.5x elapsed time, but also won't accept a test
-  skip_if_not(interactive())
-  skip_on_ci()
-  skip_on_cran()
-  timer <- system.time({
-    con <- stage_conn()
-    strike_stage(con)
-  })
+# test_that("stage_conn will pass CRAN", {
+#   # cran requires user time <= 2.5x elapsed time, but also won't accept a test
+#   skip_if_not(interactive())
+#   skip_on_ci()
+#   skip_on_cran()
+#   timer <- system.time({
+#     con <- stage_conn()
+#     strike_stage(con)
+#   })
 
-  expect_lte(timer[["user.self"]] / timer[["elapsed"]], 2.5)
-})
+#   expect_lte(timer[["user.self"]] / timer[["elapsed"]], 2.5)
+# })
